9f576535c794bd029786b8d1c5a25b59fd0df04c
BranHnrqz <86216647+BranHnrqz@users.noreply.github.com>
2021-08-17 09:26:06 -0600

    TercerCommit

--- Persona/Persona/Negocio/NClsPersona.cs
+++ Persona/Persona/Negocio/NClsPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Persona.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace Persona.Negocio
 {
     class NClsPersona
     {
+        public String Hablar(ClsPersona persona)
+        {
+            return "La persona con nombre " + persona.Nombre + " Dice Hola ";
+        }
+        public String Caminar(ClsPersona persona)
+        {
+            return "La persona " + persona.Nombre + " va Caminando ";
+        }
+        public String Reir(ClsPersona persona)
+        {
+            return "La persona " + persona.Nombre + " Está Riendo ";
+        }
+        public String Observar(ClsPersona persona)
+        {
+            return "La persona " + persona.Nombre + " Está Observando ";
+        }
     }
 }
